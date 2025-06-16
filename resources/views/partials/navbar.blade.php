445b98b95caec753fd00e55c73a1103f53784f4c
jogindaryadav <jogindary426@gmail.com>
2025-06-16 18:54:16 +0545

    darktoggle

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -10,9 +10,15 @@
             <a href="">Login</a>
         </li>
         <li><a href="" class="hover:text-[#e35f0e]">Register</a></li>
-        <button class="rounded-full border-[1px] border-black dark:border-white relative" id="darkMode">
-            <img src="{{ asset('images/moon.svg') }}" id="icon-moon" class="block dark:hidden px-[5px] py-[6px] h-[2vw] w-[2vw]">
-            <img src="{{ asset('images/sun.png') }}" id="icon-sun" class="hidden dark:block px-[5px] py-[6px] h-[2vw] w-[2vw]">
+        <button id="darkToggle" class="border p-2 rounded-full">
+            <svg id="sunIcon" class="w-6 h-6 hidden dark:block" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M12 3v1m0 16v1m8.66-13.66l-.71.71M4.05 19.95l-.71.71M21 12h-1M4 12H3m16.66 4.95l-.71-.71M4.05 4.05l-.71-.71M16 12a4 4 0 11-8 0 4 4 0 018 0z"/>
+            </svg>
+            <svg id="moonIcon" class="w-6 h-6 block dark:hidden" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"/>
+            </svg>
         </button>
     </ul>
 </div>
