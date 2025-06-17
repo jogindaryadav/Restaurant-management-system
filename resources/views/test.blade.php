635ca9de086063a25fb60abdfc677263384242f1
jogindaryadav <jogindary426@gmail.com>
2025-06-17 17:12:25 +0545

    searchInput change

---
+++ resources/views/test.blade.php
@@ -0,0 +1,16 @@
+const searchInput = document.getelElementById('searchInput');
+const items-card = document.querrySlecterAll('items-card');
+
+searchInput.addEventListner("input" , () function{
+    const serchTerm = this.value.toLowerCase();
+
+    items.forEach(item =>{
+        const itemName = document.querySelector(".itemName").textContent.toLowerCase();
+        if(itemName.include(searchTerm));{
+            item.classList.remove("hidden");
+        }
+        else{
+            item.classList.add("hidden");
+        }
+    });
+});
